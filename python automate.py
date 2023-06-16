ae4bb55c9ad8720ce9c3915efec3af56d32f5762
wizzy <williamsthebrains@gmail.com>
2023-06-16 14:03:25 +0000

    second second

---
+++ python automate.py
@@ -0,0 +1,10 @@
+# Python Proofreading
+# pip install lmproof
+import lmproof
+def proofread(text):
+    proofread = lmproof.load("en")
+    correction = proofread.proofread(text)
+    print("Original: {}".format(text))
+    print("Correction: {}".format(correction))
+    
+proofread("Your Text")
